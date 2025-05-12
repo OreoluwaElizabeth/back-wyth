eba44a433bbb761c7f97cc40b298f921a676c498
OreoluwaElizabeth <oreoluwaadeyemo77@gmail.com>
2025-05-12 17:58:02 +0100

    New stuffs

--- src/components/Smartphone.jsx
+++ src/components/Smartphone.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useState } from "react-router-dom";
+import { useState } from "react";
 
 const Smartphone = () => {
     const [input, setInput] = useState("");
